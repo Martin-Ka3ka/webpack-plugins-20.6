19de0bfc3265622b15fb75eebe7dbf205155cfe1
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-05-02 17:55:15 +0200

    add modified files

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,8 @@ import Todo from './Todo.js';
 import style from './TodoList.css';
 
 const TodoList = props => {
-    const list = props.datal.map((data) => <Todo text={data.text} id={data.id} key={data.id} remove={props.remove.bind(this)} />);
+    const list = props.data.map((data) => <Todo text={data.text} id={data.id} key={data.id} remove={props.remove.bind(this)} />);
+
 
     return (
         <ul className={style.dataList} id={'todoList'}>
